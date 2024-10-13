5eb4bbaaa54b45e43aa4b34abc444784666cf382
dhafdelin <daniel_hafdelin@outlook.com>
2024-10-13 11:42:14 -0700

    Update Last_3_Cust_Ords.sql

--- Last_3_Cust_Ords.sql
+++ Last_3_Cust_Ords.sql
@@ -16,7 +16,7 @@ OrderNum	OrderDate	CustName		Order_Total
 1102395		2018-04-04	Abernathy LLC		194.61
 1103850		2018-10-26	Abernathy LLC		5870.20
 1104205		2018-12-19	Abernathy LLC		191.11
-1102803		2018-06-04	Abra Lewis			24.99
-1104454		2019-01-25	Abra Lewis			108.00
-1104784		2019-03-14	Abra Lewis			54.00
+1102803		2018-06-04	Abra Lewis		24.99
+1104454		2019-01-25	Abra Lewis		108.00
+1104784		2019-03-14	Abra Lewis		54.00
 */
