e60b5f68f10aebd708fdcc6b3769408ba7630e66
dhafdelin <daniel_hafdelin@outlook.com>
2024-10-13 11:43:27 -0700

    Update Self_Join.sql

--- Self_Join.sql
+++ Self_Join.sql
@@ -9,13 +9,13 @@ FROM [EmployeeDirectory] ED1
 Order by Manager_Name,Employee_Name
 /*
 Manager_Name		Manager_Title				Employee_Name			Employee_Title
-Amaya Witt			Customer Service Manager	Ainsley Barnett			Customer Service Representative
-Amaya Witt			Customer Service Manager	Amelia Mcfadden			Customer Service Representative
-Amaya Witt			Customer Service Manager	Jemima Mccormick		Customer Service Representative
-Amaya Witt			Customer Service Manager	Julie Davenport			Customer Service Representative
-Amaya Witt			Customer Service Manager	Medge Rollins			Customer Service Representative
-Amaya Witt			Customer Service Manager	Thor Watson				Customer Service Representative
-Amethyst Ashley		VP Customer Service			Amaya Witt				Customer Service Manager
+Amaya Witt		Customer Service Manager		Ainsley Barnett			Customer Service Representative
+Amaya Witt		Customer Service Manager		Amelia Mcfadden			Customer Service Representative
+Amaya Witt		Customer Service Manager		Jemima Mccormick		Customer Service Representative
+Amaya Witt		Customer Service Manager		Julie Davenport			Customer Service Representative
+Amaya Witt		Customer Service Manager		Medge Rollins			Customer Service Representative
+Amaya Witt		Customer Service Manager		Thor Watson			Customer Service Representative
+Amethyst Ashley		VP Customer Service			Amaya Witt			Customer Service Manager
 Amethyst Ashley		VP Customer Service			Guinevere Banks			Engineer
 Amethyst Ashley		VP Customer Service			Isabella Bauer			Customer Service Representative
 Amethyst Ashley		VP Customer Service			Kiona Hancock			Customer Service Manager
